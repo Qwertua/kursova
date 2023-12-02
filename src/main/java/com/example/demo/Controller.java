a51bdb49e1018b53e79a61d3ed9aef53e479c548
Qwertua <wertua17062004@gmail.com>
2023-12-02 14:15:59 +0200

    First step of working project + adding historyButton + refreshButton + zoomIm/zoomOut

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
@@ -7,6 +8,7 @@ import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebHistory;
 import javafx.scene.web.WebView;
 
 
@@ -17,14 +19,47 @@ public class Controller implements Initializable {
     @FXML
     private TextField textField;
     private WebEngine engine;
+    private String homePage;
+    private double webZoom;
+    private WebHistory history;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        homePage = "www.google.com";
         engine = webView.getEngine();
+        textField.setText(homePage);
+        webZoom = 1;
+
         loadPage();
+
+
 
     }
     public void loadPage(){
+        /*engine.load("http://www.google.com");*/
+        engine.load("http://" + textField.getText());
 
     }
+    public void refreshPage(){
+        engine.reload();
+    }
+    public void zoomIn(){
+        webView.setZoom(webZoom);
+        webZoom += 0.25;
+
+    }
+    public void zoomOut(){
+        webView.setZoom(webZoom);
+        webZoom -= 0.25;
+
+    }
+    public void displayHistory(){
+        history = engine.getHistory();
+        ObservableList<WebHistory.Entry> entries = history.getEntries();
+        for(WebHistory.Entry entry : entries){
+            System.out.println(entry);
+        }
+
+    }
+
 
 }
--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scene.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);stage.setTitle("WebBrowser!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);stage.setTitle("WebBrowser!");
         stage.setScene(scene);
         stage.show();
     }
