cdf63de18b1d995fbad4a7004332285f347f3748
Qwertua <wertua17062004@gmail.com>
2023-12-03 13:37:48 +0200

    Adding dynamic backButton and ForwardButton + adding methodUpdateButtonStatus with disable method + adding Listener which will initialize history fter the page has been loaded

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,19 +1,24 @@
 package com.example.demo;
 
 import javafx.collections.ObservableList;
+
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebHistory;
 import javafx.scene.web.WebView;
 
-
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    @FXML
+    private Button forwardButton;
+    @FXML
+    private Button backButton;
     @FXML
     private WebView webView;
     @FXML
@@ -21,67 +26,90 @@ public class Controller implements Initializable {
     private WebEngine engine;
     private String homePage;
     private double webZoom;
-    private WebHistory history;
+    private WebHistory history ;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         homePage = "www.google.com";
         engine = webView.getEngine();
         textField.setText(homePage);
+
         webZoom = 1;
+
+        // Load the page and initialize history when the page is loaded
+        engine.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == Worker.State.SUCCEEDED) {
+                history = engine.getHistory();
+
+                UpdateButtonStatus();
+            }
+        });
+
         loadPage();
-
     }
-    public void loadPage(){
 
+    public void loadPage() {
         engine.load("http://" + textField.getText());
-
+        engine.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == Worker.State.SUCCEEDED) {
+                // Page has been loaded, now get the history
+                history = engine.getHistory();
+                System.out.println(history.getCurrentIndex());
+                UpdateButtonStatus();
+            }
+        });
     }
-    public void refreshPage(){
+
+    public void refreshPage() {
         engine.reload();
     }
-    public void zoomIn(){
+
+    public void zoomIn() {
         webView.setZoom(webZoom);
         webZoom += 0.25;
-
     }
-    public void zoomOut(){
+
+    public void zoomOut() {
         webView.setZoom(webZoom);
         webZoom -= 0.25;
-
     }
-    public void displayHistory(){
+
+    public void displayHistory() {
         history = engine.getHistory();
         ObservableList<WebHistory.Entry> entries = history.getEntries();
-        for(WebHistory.Entry entry : entries){
-            System.out.println(entry.getUrl()+ " " + entry.getLastVisitedDate());
+        for (WebHistory.Entry entry : entries) {
+            System.out.println(entry.getUrl() + " " + entry.getLastVisitedDate());
         }
-
     }
-    public void goBack(){
+
+    public void goBack() {
         history = engine.getHistory();
         ObservableList<WebHistory.Entry> entries = history.getEntries();
         int index = history.getCurrentIndex();
-        if (index > 0 ){
+        if (index > 0) {
             history.go(-1);
             textField.setText(entries.get(history.getCurrentIndex()).getUrl());
-
         }
-
+        UpdateButtonStatus();
     }
-    public void goForward(){
+
+    public void goForward() {
         history = engine.getHistory();
         ObservableList<WebHistory.Entry> entries = history.getEntries();
         int index = history.getCurrentIndex();
-        if (index > entries.size()-1){
+        if (index < entries.size() - 1) {
             history.go(1);
             textField.setText(entries.get(history.getCurrentIndex()).getUrl());
-
         }
-
+        UpdateButtonStatus();
     }
-  /* public void executeJS(){
-        engine.executeScript("window.location = \"https://www.youtube.com\";");
 
-    }*/
+    public void UpdateButtonStatus() {
+        int currentIndex = history.getCurrentIndex();
+        int totalEntries = history.getEntries().size();
 
+        backButton.setDisable(currentIndex <= 0);
+        forwardButton.setDisable(currentIndex >= totalEntries - 1);
+    }
 }
